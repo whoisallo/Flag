0fd62bffcc6a79b5e201c4d7f1a3ef888056040f
Matthew Price <doggy1199@yahoo.com>
2013-02-14 07:57:21 -0700

    created GraphicsPoint

---
+++ GraphicsPoint.java
@@ -0,0 +1,22 @@
+/**
+ * GraphicsPoint
+ *
+ * 		a point that is limited to positive integers
+ */
+
+class GraphicsPoint {
+	
+	private int x, y;
+
+	/**
+	 * Constructor
+	 *
+	 * @param x - x coordinate of point
+	 * @param y - y coordinate of point
+	 */
+	public GraphicsPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
