a28d9b021d65d92051a94ab54cf44ac028d0f77f
Matthew Price <doggy1199@yahoo.com>
2013-02-14 07:49:07 -0700

    Added block comment to constructor

--- Point.java
+++ Point.java
@@ -7,6 +7,12 @@ class Point {
 	
 	public int x, y;
 
+	/**
+	 * Constructor
+	 *
+	 * @param x - x coordinate of point
+	 * @param y - y coordinate of point
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
